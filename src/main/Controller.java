7277491d20fcc5f764d593825d4e15eceede9a3f
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2016-12-08 02:36:44 -0300

    Added Singleton design to Controller¨

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,5 +1,23 @@
 package main;
 
+/*
+* Nomad-j
+* @author Paulo R. Almeida Filho
+* @email palmeidaprogramming@gmail.com
+*/
+
 public class Controller {
+
+    //--Singleton design----------------------------------
+    private volatile static Controller instance = null;
+    private Controller() { }
+    public synchronized static Controller getInstance() {
+        if(instance == null) {
+            instance = new Controller();
+        }
+        return instance;
+    }
+
+    //--Events--------------------------------------------
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,21 +1,39 @@
 package main;
 
+/*
+* Nomad-j
+* @author Paulo R. Almeida Filho
+* @email palmeidaprogramming@gmail.com
+*/
+
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    public static Parent root;
+    public static Stage mainStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
+        // FXML Load
+        FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        mainLoader.setController(Controller.getInstance());
+        try {
+            root = mainLoader.load();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        mainStage = primaryStage; // saves Stage as public
+        primaryStage.setTitle("Nomad");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
