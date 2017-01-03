6a413ddf142493ac4b7a0fac36edb2015148120e
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2017-01-03 08:18:20 -0300

    Sync save

--- src/com/github/palmeidaprog/nomad/main/Controller.java
+++ src/com/github/palmeidaprog/nomad/main/Controller.java
@@ -6,14 +6,17 @@ package com.github.palmeidaprog.nomad.main;
 * @email palmeidaprogramming@gmail.com
 */
 
+import com.github.palmeidaprog.nomad.sync.Profile;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
+import javafx.scene.control.TableView;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.effect.Effect;
 import javafx.scene.image.Image;
@@ -26,12 +29,15 @@ import javafx.util.Duration;
 import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static com.github.palmeidaprog.nomad.main.Main.mainStage;
 
-public class Controller {
+public class Controller implements Initializable {
 
     @FXML public ImageView addImage, editImage, deleteImage;
+    @FXML private TableView<Profile> profileTable;
     private int i = 1;
     private static final double W = 330;
     private static final double H = 590;
@@ -53,6 +59,13 @@ public class Controller {
             instance = new Controller();
         }
         return instance;
+    }
+
+    //--Implementing iniatilizable interface---------------------
+
+    @Override
+    public void initialize(URL u,ResourceBundle rb) {
+        profileTable.requestFocus(); // todo: Find a way to stop TextField requesting focus
     }
 
     //--Enter Events--------------------------------------------
